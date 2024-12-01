fe811f9fea0c4306ba6b9f9c6c03d175753ee8ca
Phann020126 <daniel020126@gmail.com>
2024-12-01 17:40:02 -0500

    first crawler

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -1,8 +1,14 @@
 import requests
+import re
+
+from queue import Queue
 from bs4 import BeautifulSoup
 
-def fetch_page(url):
-    """Realiza una solicitud GET a la URL y devuelve el contenido de la página."""
+from src.crawler.utils import ForumPage
+
+
+def fetch_page(url:str) -> str | None:
+
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -11,19 +17,52 @@ def fetch_page(url):
         print(f"Error al acceder a {url}: {e}")
         return None
     
-def parse_page(html):
-    """Analiza el contenido HTML y extrae los links."""
+def parse_page(html:str, page:ForumPage) -> list[ForumPage]:
 
     soup = BeautifulSoup(html, "html.parser")
 
     links = []
     for link in soup.find_all('a', href=True):
-        links.append(link['href'])
+        
+        url = link['href']
+        
+        new_page = ForumPage(url) if page.is_category() else ForumPage(url, page.main_class, page.sub_class)
+        
+        links.append(new_page)
 
     return links
 
-def save_html(content, filename):
-    """Save the HTML content to a file."""
+def save_html(content:str, page:ForumPage):
 
-    with open(filename, "w", encoding='utf-8') as f:
+    if not page.is_topic():
+        return
+
+    folder = f"./{page.main_class}/{page.sub_class}/"
+    filename = "_".join(page.url.split("/")[-2:])
+
+    with open(folder + filename, "w", encoding='utf-8') as f:
         f.write(content)
+
+def download_web_page(page:ForumPage, visited_urls:set) -> list[ForumPage]:
+
+    if page.url not in visited_urls and page.url:
+        visited_urls.add(page.url)
+        
+        response = fetch_page(page.url)
+        
+        if response == None:
+            return []
+        
+        save_html(response, page)
+        
+        return parse_page(response, page)
+
+def main(first_page:str, visited_urls:set[str]):
+
+    q = Queue(0)
+    q.put(ForumPage(first_page))
+
+    while not q.empty:
+        url = q.get()
+        for x in download_web_page(url, visited_urls):
+            q.put(x)
---
+++ src/crawler/utils.py
@@ -0,0 +1,31 @@
+import re
+
+patterns = {
+    "https://us.forums.blizzard.com/en/wow/c/": "category",
+    "https://us.forums.blizzard.com/en/wow/t/": "topic",
+}
+
+class ForumPage:
+    def __init__(self, url, main_class = None, sub_class = None):
+        self.url = url
+        self.type = "other"
+        self.main_class = main_class
+        self.sub_class = sub_class
+        self._set_attributes()
+
+    def _set_attributes(self):
+        
+        for key in patterns.keys():
+            if re.match(key, self.url):
+                self.type = patterns[key]
+
+        if self.is_category():
+            split_url = self.url.split("/")
+            self.main_class = split_url[-3]
+            self.sub_class = split_url[-2]
+
+    def is_topic(self):
+        return self.type == "topic"
+    
+    def is_category(self):
+        return self.type == "category"
